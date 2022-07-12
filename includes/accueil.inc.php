142cf58f9e066b63d901c977b50c01f8f4eec185
SamTravail <105202149+SamTravail@users.noreply.github.com>
2022-07-12 14:21:59 +0200

    add css
    add css

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,1 +1,10 @@
 <h1>Accueil</h1>
+
+<div class="card">
+
+    <div class="card">
+        Magic Card
+    </div>
+
+    <a href="https://mythrillfiction.com/" target="_blank">Mythrill</a>
+</div>
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,20 @@
-<h1>Login</h1>
+<div class="login-box">
+    <h2>Login</h2>
+    <form>
+        <div class="user-box">
+            <input type="text" name="" required="">
+            <label>Username</label>
+        </div>
+        <div class="user-box">
+            <input type="password" name="" required="">
+            <label>Password</label>
+        </div>
+        <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Envoyer
+        </a>
+    </form>
+</div>
